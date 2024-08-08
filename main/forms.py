05163bbde0f0f8c7c0fef9c8eb1636e266b655be
mreider <mike.reider@gmail.com>
2024-08-08 11:17:28 -0400

    hostgroup

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,6 @@
 from django import forms 
 from .models import HostGroup, Host
-
+from loguru import logger
 
 class HostGroupForm(forms.ModelForm):
     class Meta:
@@ -9,11 +9,22 @@ class HostGroupForm(forms.ModelForm):
 
     name = forms.CharField(max_length=40, required=True)
     description = forms.CharField(max_length=50, required=False)
-    host = forms.MultipleChoiceField(required=False, widget=forms.CheckboxSelectMultiple)
+    host = forms.MultipleChoiceField(required=False, widget=forms.CheckboxSelectMultiple())
 
     def __init__(self, *args, **kwargs):
         super(HostGroupForm, self).__init__(*args, **kwargs)
         self.fields['host'].choices = [(host.monit_id, host.name) for host in Host.objects.filter(approved=True)]
+    
+   
+       # If an instance is provided, set the initial values for the host field
+    #    if 'instance' in kwargs:
+    #        logger.warning(kwargs["instance"])
+   
+                
+            #self.fields['host'].initial = [host.monit_id for host in kwargs['instance'].host.all()]
+   #         logger.debug(self.fields['host'].initial)
+
+    
     # def save(self, commit=True):
     #     hostgroup = super().save()
     #     if commit:
